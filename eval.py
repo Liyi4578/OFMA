80f9b17bfc9612b20e6e37b7813d7d2866ca6ac4
ShaiYueLiangLo <57708904+Liyi4578@users.noreply.github.com>
2023-08-21 12:38:25 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -10,6 +10,9 @@ from utils.functions import get_model_info,gpu_mem_usage,show_box_on_img
 from data.detrac_dataset import DetracDataset
 from data.data_transform import ValTransform
 
+# 该文件不会直接生成结果，而是生成 UA-DETRAC 评估工具需要的 txt格式
+# 具体的 结果需要使用  UA-DETRAC 官方评估工具
+# 注意，因为训练方式的关系，生成的结果中可能有重复结果，需要对每帧的结果重新 nms 一次。
 
 def main():
     logger.remove()
@@ -37,8 +40,8 @@ def main():
     model.load_state_dict(ckpt["model"])
     
     evaluator.evaluate(model,half = False,return_outputs = False)
-    logger.info("-------ap50:{},ap50_95:{}-------",ap50,ap50_95)
-    logger.info("\n" + summary)
+    //logger.info("-------ap50:{},ap50_95:{}-------",ap50,ap50_95)
+    //logger.info("\n" + summary)
     
 
 if __name__ == '__main__':
